49b4d18fbdc741d53938035afc2551e1bc6edfd1
burakkarabiyik <burakkarabiyik@mail.com.tr>
2017-03-21 13:46:58 +0300

    Mvc Geçiş
    Mvc

---
+++ KisilikTesti/Controllers/HomeController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KisilikTesti.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Giris()
+        {
+            return View();
+        }
+        public ActionResult Intro()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+        public ActionResult TavsiyeMavi()
+        {
+            return View();
+        }
+        public ActionResult TavsiyeSari()
+        {
+            return View();
+        }
+        public ActionResult TavsiyeKirmizi()
+        {
+            return View();
+        }
+        public ActionResult TavsiyeYesil()
+        {
+            return View();
+        }
+
+        public ActionResult sonuc()
+        {
+            return View();
+        }
+        public ActionResult kirmizi()
+        {
+            return View();
+        }
+        public ActionResult sari()
+        {
+            return View();
+        }
+        public ActionResult yesil()
+        {
+            return View();
+        }
+        public ActionResult mavi()
+        {
+            return View();
+        }
+        public ActionResult Soru1()
+        {
+            
+            return PartialView();
+
+        }
+        public ActionResult Soru2()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru3()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru4()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru5()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru6()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru7()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru8()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru9()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru10()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru11()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru12()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru13()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru14()
+        {
+            return PartialView();
+
+        }
+        public ActionResult Soru15()
+        {
+            return PartialView();
+
+        }
+
+
+        public ActionResult Contact()
+        { 
+
+            return View();
+        }
+    }
+}
---
+++ KisilikTesti/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KisilikTesti.MvcApplication" Language="C#" %>
